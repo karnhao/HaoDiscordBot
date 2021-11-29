7bd042acb47b4e3e606d7b02429b432660d9d0f7
karnhao <karn0888422952@gmail.com>
2021-11-29 09:17:10 +0700

    starting-add playsound command

---
+++ commands/playsound.js
@@ -0,0 +1,12 @@
+import { Message } from "discord.js";
+
+export const name = 'playsound';
+export const description = 'เล่นเสียง';
+/**
+ * 
+ * @param {Message} message 
+ * @param {String[]} args 
+ */
+export async function execute(message, args) {
+    
+}
