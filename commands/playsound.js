78e936ee46d8427f03c95ad6f23335ecfcb2e419
karnhao <karn0888422952@gmail.com>
2021-11-29 12:11:03 +0700

    11/29/2021 12:11 PM

--- commands/playsound.js
+++ commands/playsound.js
@@ -1,12 +1,37 @@
-import { Message } from "discord.js";
+import { Message, VoiceChannel } from "discord.js";
+import { client } from "../main.js";
+import { createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus, generateDependencyReport } from "@discordjs/voice";
 
 export const name = 'playsound';
 export const description = 'เล่นเสียง';
+
 /**
  * 
  * @param {Message} message 
  * @param {String[]} args 
  */
 export async function execute(message, args) {
-    
+    try {
+        console.log(generateDependencyReport());
+        const vc = message.guild.channels.cache.get(args[0]);
+        if (!vc || !vc.isVoice()) throw new Error("ไม่พบห้องพูดคุย");
+        let permission = vc.permissionsFor(client.user);
+        if (!permission.has("CONNECT")) throw new Error("ไม่มีสิทธิในการเข้าห้องนั้น");
+        if (!permission.has("SPEAK")) throw new Error("ไม่มีสิทธิในการพูดในห้องนั้น");
+        if (message.attachments?.first()?.url == null) throw new Error("ไม่มีข้อมูล");
+        let player = createAudioPlayer();
+        let resourse = createAudioResource(message.attachments.first().url);
+        let connection = joinVoiceChannel({
+            channelId: vc.id,
+            guildId: message.guildId,
+            adapterCreator: message.guild.voiceAdapterCreator
+        });
+        connection.subscribe(player);
+        player.play(resourse);
+        player.on(AudioPlayerStatus.Idle, () => {
+            connection.destroy();
+        });
+    } catch (e) {
+        await message.channel.send({ content: "❌มีบางอย่างผิดพลาด : " + e });
+    }
 }
